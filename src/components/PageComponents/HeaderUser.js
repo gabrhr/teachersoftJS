145055bdf433bcfb81420e56af40d5512cb516aa
gabrhr <73925454+gabrhr@users.noreply.github.com>
2021-10-09 09:37:18 -0500

    Add SideBar Drawer
    Eliminar DTButton, Añadir resposive SideBar

--- src/components/PageComponents/HeaderUser.js
+++ src/components/PageComponents/HeaderUser.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { makeStyles } from '@mui/styles';
 import { AppBar, Grid, IconButton, Button, Toolbar, Divider,  Avatar} from '@mui/material';
-import { Typography, List, ListItem,ListItemIcon ,Box,Drawer } from '@mui/material';
+import { Typography, List, ListItem,ListItemIcon ,Box } from '@mui/material';
 import MuiDrawer from '@mui/material/Drawer'
 import DehazeIcon from '@mui/icons-material/Dehaze';
 import SearchIcon from '@mui/icons-material/Search';
-import DTButton from '../GeneralComponents/DTButton';
 import logout from '../../assets/images/log-out.png'
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
@@ -14,6 +13,9 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { styled, useTheme } from '@mui/material/styles';
+import LogoPucp from '../../assets/images/LogoPUCP.png'
+
+import Header from './Header';
 
 const useStyles = makeStyles(themex => ({
     root: {
@@ -41,25 +43,80 @@ const useStyles = makeStyles(themex => ({
     },
     gridSpace:{
         marginTop:themex.spacing(0)
+    },
+    menuImagen: {
+        position: 'relative',
+        height: '45px'
     }
 }))
+
+const pagina = makeStyles({
+    appMain: {
+      paddingLeft: '320px',
+      width: '100%'
+    }
+  })
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 5px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(9)} + 9px)`,
+  },
+});
+
 const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  }));
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(8.2, 8),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+ );
+  
 
 export default function HeaderUser(props) {
     const classes = useStyles();
+    const cls= pagina();
     const {nombre, rol, foto} = props;
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
-    setOpen(true);
+    setOpen(!open);
     console.log(true)
   };
 
@@ -70,12 +127,32 @@ export default function HeaderUser(props) {
 
     return (
         <Box sx={{ display: 'flex' }}>
-            <AppBar position="static" 
+            <AppBar position="fixed" 
                 sx={{
+                    bgcolor: 'primary',
+                    boxShadow: 1,
+                    transform: 'translateZ(0)',
+                    zIndex:  (theme) => theme.zIndex.drawer + 1
+                }}>
+                <Toolbar>
+                    <Grid container
+                        alignItems="center">
+                        <Grid item sm>
+                        </Grid>
+                        <Grid item>
+                        <img className={classes.menuImagen} src= {LogoPucp} alt=""></img>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+            <AppBar 
+                sx={{
+                    marginTop:"65px",
                     bgcolor: '#fff',
                     boxShadow: 1,
-                    transform: 'translateZ(0)'
-                }}>
+                    transform: 'translateZ(0)',
+                    zIndex: (theme) => theme.zIndex.drawer + 1
+                }} position="absolute">
                 <Toolbar>
                     <Grid container
                         direction="row"
@@ -113,25 +190,25 @@ export default function HeaderUser(props) {
                     </Grid>
                 </Toolbar>
             </AppBar>
-            <Drawer  open={open} > 
-                <DrawerHeader>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider />
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <div className={cls.appMain}> 
+                <Drawer
+                    variant="permanent"
+                    elevation = {1}
+                    open = {open}
+                    >
+                    <DrawerHeader/>
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                         <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
                         </ListItem>
-                    ))}
-                </List>
-                <Divider />
-            </Drawer>
-        </Box>
+                        ))}
+                    </List>
+                </Drawer>
+            </div>
+       </Box>
     );
 }
