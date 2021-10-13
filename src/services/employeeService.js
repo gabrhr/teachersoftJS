4d50d1f315420144a92add50bc60fb714573e49c
CodeBreakerMG <70769438+CodeBreakerMG@users.noreply.github.com>
2021-10-13 00:48:39 -0500

    Delete src directory

--- src/services/employeeService.js
+++
@@ -1,8 +0,0 @@
-export const getDepartmentCollection = () => ([
-    { id: '1', title: 'Development' },
-    { id: '2', title: 'Marketing' },
-    { id: '3', title: 'Accounting' },
-    { id: '4', title: 'HR' },
-    { id: '5', title: 'Ventas' },
-    { id: '6', title: 'Gerencia' }
-])
