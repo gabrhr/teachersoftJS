9637f0e05ef4fd71517472307b66d951de008863
erickOz <erickolivareszea@gmail.com>
2021-10-14 03:45:39 -0500

    version De Enrutamiento dinámico Id Roles inicio sesión

--- src/App/App.js
+++ src/App/App.js
@@ -14,6 +14,7 @@ import {ThemeProvider} from '@mui/material/styles';
 import theme from './theme.js'
 import HeaderUser from '../components/PageComponents/HeaderUser';
 import fotoUsuario from '../assets/images/profile-photo.png'
+import { Route, Switch } from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
 // import {MenuAdministrador} from '../components/PageComponents/MenuAdministrador';
 import Login from '../pages/Login/Login';
@@ -21,7 +22,7 @@ import Login from '../pages/Login/Login';
 
 function App() {
   /* PRUEBAS (solo util para probarl login screen) */
-  if (false) {
+  if (true){
     return (
       <ThemeProvider theme={theme}>
         <Router>
@@ -31,20 +32,57 @@ function App() {
       </ThemeProvider>
     )
   }
-
   return (
     <ThemeProvider theme={theme}>
-      <Router >
-        <HeaderUser
-              nombre="New Employee"
-              rol="Administrador"
-              idRol= {1}
-              foto={fotoUsuario}
-        />
-        <CssBaseline />
+      <Router forceRefresh={false}>
+        <div clasName = "classes.root" >
+          <Switch>
+            <Route
+              exact
+              path = "/"
+              idRoles = {[2, 5]}
+            />
+            <Route
+              exact
+              path = "/Administrador/mantenimiento"
+              idRoles = {[1, 3]}
+              //component = { (props) => gestionar({
+              //...props.location.state,
+              //}) }
+            />
+            <Route
+              exact
+              path = "activate/:token"
+              //component = {ActivateLogin}
+            />
+            <Router
+              exact
+              path = "/resetPassword/:email/:token"
+              //component = {ResetPassword}
+            />
+            <Route
+              exact
+              path = "/NoRoles"
+              //component = {NoRoles}
+            />
+            <Route exact path = "/" component = {Login}/>
+            <Route exact path = "/login/:thing?" component = {Login} />
+            <Route exact path = "/prueba/1"/>
+            <Route default component = {Login} />
+          </Switch>
+          /*
+          <HeaderUser
+            nombre="New Employee"
+            rol="Administrador"
+            idRol= {1}
+            foto={fotoUsuario}
+          />
+          */
+          <CssBaseline />
+        </div>
         </Router>
-    </ThemeProvider>
-  );
+      </ThemeProvider>
+      );
 }
 
 export default App;
