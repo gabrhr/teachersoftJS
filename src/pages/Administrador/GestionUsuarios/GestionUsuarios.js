319798c2104bde1d299e1a9914442c40f8e4f6d4
mitsuo <rtokumori@pucp.edu.pe>
2021-10-14 11:48:41 -0500

    Add funcionalidad Gestion Usuarios
    - Falta validacion de data.

--- src/pages/Administrador/GestionUsuarios/GestionUsuarios.js
+++ src/pages/Administrador/GestionUsuarios/GestionUsuarios.js
@@ -5,7 +5,6 @@ import { Box, Paper, TableBody, TableRow, TableCell } from '@mui/material';
 import { Controls } from '../../../components/controls/Controls';
 import { useForm, Form } from '../../../components/useForm';
 import ContentHeader from '../../../components/AppMain/ContentHeader';
-import * as employeeService from '../../../services/employeeService';
 import { Avatar, Divider, Grid, Stack, Typography } from '@mui/material'
 import { DT } from '../../../components/DreamTeam/DT'
 import Popup from '../../../components/util/Popup'
@@ -13,44 +12,14 @@ import useTable from "../../../components/useTable"
 import GestionUsuariosForm from './GestionUsuariosForm'
 import Notification from '../../../components/util/Notification'
 import ConfirmDialog from '../../../components/util/ConfirmDialog'
+/* SERVICES */
+import * as personaService from '../../../services/personaService'
+import * as DTLocalServices from '../../../services/DTLocalServices';
 /* ICONS */
 import SearchIcon from '@mui/icons-material/Search';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import CloseIcon from '@mui/icons-material/Close';
 
-/* DATA */
-const getSecciones = () => ([
-  { id: 1, title: 'Informatica' },
-  { id: 2, title: 'Telecomunicaciones' },
-  { id: 3, title: 'Industrial' },
-  { id: 4, title: 'Civil' },
-  { id: 5, title: 'Mecanica' },
-  { id: 6, title: 'Fisica' }
-])
-
-/* create labeled data */
-function createData(id, fullName, seccion, departamento, dni, email,) {
-  return {
-    id,
-    fullName,
-    seccion,
-    departamento: departamento + ' ' + seccion,
-    dni,
-    email
-  }
-}
-
-const usuarios2 = [
-  createData('0', 'Nombre1', 'Ing. Informatica', 'FCI', '12345678', 'asdf@example.com'),
-  createData('1', 'Nombre2', 'Ing. Informatica', 'FCI', '12345678', 'asdf@example.com'),
-  createData('2', 'Nombre3', 'Ing. Informatica', 'FCI', '12345678', 'asdf@example.com'),
-  createData('3', 'Nombre4', 'Ing. Informatica', 'FCI', '12345678', 'asdf@example.com'),
-  createData('4', 'Nombre5', 'Ing. Informatica', 'FCI', '12345678', 'asdf@example.com'),
-  createData('5', 'Nombre6', 'Ing. Informatica', 'FCI', '12345678', 'asdf@example.com'),
-  createData('6', 'Nombre7', 'Ing. Informatica', 'FCI', '12345678', 'asdf@example.com'),
-]
-
-/* para que seleccione seccion */
 const initialFieldValues = {
   seccionID: '',
 }
@@ -63,18 +32,6 @@ const tableHeaders = [
     sortable: true
   },
   {
-    id: 'seccion',
-    label: 'Sección',
-    numeric: false,
-    sortable: true
-  },
-  {
-    id: 'departamento',
-    label: 'Departamento',
-    numeric: false,
-    sortable: true
-  },
-  {
     id: 'dni',
     label: 'DNI',
     numeric: false,
@@ -82,7 +39,25 @@ const tableHeaders = [
   },
   {
     id: 'email',
-    label: 'Correo Electrónico',
+    label: 'Correo',
+    numeric: false,
+    sortable: true
+  },
+  {
+    id: 'rol',
+    label: 'rol',
+    numeric: false,
+    sortable: true
+  },
+  {
+    id: 'seccion',
+    label: 'Seccion',
+    numeric: false,
+    sortable: true
+  },
+  {
+    id: 'departamento',
+    label: 'Departamento',
     numeric: false,
     sortable: true
   },
@@ -98,7 +73,7 @@ export default function GestionUsuarios() {
   /* COSAS PARA LA TABLITA 
    * ===================== */
 
-  const [records, setRecords] = useState(usuarios2)
+  const [records, setRecords] = useState(DTLocalServices.getAllPersonas())
   /* no filter function initially */
   const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
   const [openPopup, setOpenPopup] = useState(false)
@@ -118,35 +93,36 @@ export default function GestionUsuarios() {
     BoxTbl
   } = useTable(records, tableHeaders, filterFn);
 
+  /* updates filter function inside `filterFn` object.  Which is used in 
+   * `useTable`'s `recordsAfterPagingAndSorting()`.  Because  */
   const handleSearch = e => {
     let target = e.target;
-    /* React "state object" (useState()) doens't allow functions, only
-     * objects.  Thus the function needs to be inside an object. */
     setFilterFn({
       fn: items => {
-        if (target.value == "")
+        if (target.value === "")
           /* no search text */
           return items
         else
-          return items.filter(x => x.fullName.toLowerCase()
-            .includes(target.value))
+          return items
+            .filter(x => x.fullName.toLowerCase()
+            .includes(target.value.toLowerCase()))
       }
     })
   }
 
   const addOrEdit = (usuario, resetForm) => {
     if (usuario.id == 0)
-      usuarios2.push(usuario)
-    // else
-    //   employeeService.updateEmployee(employee)
+      DTLocalServices.insertPersona(usuario)
+    else
+      DTLocalServices.updatePersona(usuario)
     resetForm()
     setRecordForEdit(null)
     setOpenPopup(false)
-    setRecords(employeeService.getAllEmployees())
+    setRecords(DTLocalServices.getAllPersonas())
 
     setNotify({
       isOpen: true,
-      message: 'Submitted Successfully',
+      message: 'Usuario añadido',
       type: 'success'
     })
   }
@@ -165,8 +141,8 @@ export default function GestionUsuarios() {
       isOpen: false
     })
 
-    employeeService.deleteEmployee(id)
-    setRecords(employeeService.getAllEmployees())
+    DTLocalServices.deletePersona(id)
+    setRecords(DTLocalServices.getAllPersonas())
     setNotify({
       isOpen: true,
       message: 'Deleted Successfully',
@@ -201,7 +177,7 @@ export default function GestionUsuarios() {
             label="Sección"
             value={values.seccionID}
             onChange={handleInputChange}
-            options={getSecciones()}
+            options={DTLocalServices.getAllSecciones()}
             size="medium"
           />
         </Box>
@@ -226,14 +202,11 @@ export default function GestionUsuarios() {
             onChange={handleSearch}
             type="search"
           />
-
           <Controls.AddButton
             title="Agregar Nuevo Usuario"
             variant="iconoTexto"
-
             onClick = {() => {setOpenPopup(true); setRecordForEdit(null)}}
           />
-
           {/* </Toolbar> */}
         </div>
         <BoxTbl>
@@ -241,37 +214,41 @@ export default function GestionUsuarios() {
             <TblHead />
             <TableBody>
               {
-                recordsAfterPagingAndSorting().map(item => (
-                  <TableRow key={item.id}>
-                    <TableCell>{item.fullName}</TableCell>
-                    <TableCell>{item.seccion}</TableCell>
-                    <TableCell>{item.departamento}</TableCell>
-                    <TableCell>{item.dni}</TableCell>
-                    <TableCell>{item.email}</TableCell>
-                    <TableCell>
-                      <Controls.ActionButton 
-                        color="warning"
-                        onClick={ () => {openInPopup(item)}}
-                      >
-                        <EditOutlinedIcon fontSize="small" />
-                      </Controls.ActionButton>
-                      <Controls.ActionButton 
-                        color="error"
-                        onClick={() => {
-                          // onDelete(item.id)
-                          setConfirmDialog({
-                            isOpen: true,
-                            title: '¿Eliminar usuario permanentemente?',
-                            subTitle: 'No es posible deshacer esta accion',
-                            onConfirm: () => {onDelete(item.id)}
-                          })
-                        }}
-                      >
-                        <CloseIcon fontSize="small" />
-                      </Controls.ActionButton>
-                    </TableCell>
-                  </TableRow>
-                ))
+                recordsAfterPagingAndSorting()
+                  .map(item => 
+                      (
+                        <TableRow key={item.id}>
+                          <TableCell>{item.fullName}</TableCell>
+                          <TableCell>{item.DNI}</TableCell>
+                          <TableCell>{item.correo}</TableCell>
+                          <TableCell>{item.rolName}</TableCell>
+                          <TableCell>{item.seccionName}</TableCell>
+                          <TableCell>{item.departamentoName}</TableCell>
+                          <TableCell>
+                            <Controls.ActionButton 
+                              color="warning"
+                              onClick={ () => {openInPopup(item)}}
+                            >
+                              <EditOutlinedIcon fontSize="small" />
+                            </Controls.ActionButton>
+                            <Controls.ActionButton 
+                              color="error"
+                              onClick={() => {
+                                // onDelete(item.id)
+                                setConfirmDialog({
+                                  isOpen: true,
+                                  title: '¿Eliminar usuario permanentemente?',
+                                  subTitle: 'No es posible deshacer esta accion',
+                                  onConfirm: () => {onDelete(item.id)}
+                                })
+                              }}
+                            >
+                              <CloseIcon fontSize="small" />
+                            </Controls.ActionButton>
+                          </TableCell>
+                        </TableRow>
+                      )
+                  )
               }
             </TableBody>
           </TblContainer>
@@ -284,7 +261,10 @@ export default function GestionUsuarios() {
         title="Registrar nuevo usuario"
       >
         {/* <EmployeeForm /> */}
-        <GestionUsuariosForm />
+        <GestionUsuariosForm 
+          recordForEdit={recordForEdit}
+          addOrEdit={addOrEdit}
+        />
       </Popup>
       {/* </Grid> */}
       <Notification 
