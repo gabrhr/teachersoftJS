2fcdd54ca6f5b6bc798e9beecf9f40d4b1f7e078
CodeBreakerMG <70769438+CodeBreakerMG@users.noreply.github.com>
2021-11-07 06:15:57 -0500

    update stepper con comentarios
    pequeño update stepper con comentarios

--- src/components/DreamTeam/PhaseStepper.js
+++ src/components/DreamTeam/PhaseStepper.js
@@ -107,10 +107,27 @@ const ColorlibStepIconRoot = styled('div')(({ theme, ownerState, number}) => ({
   display: 'flex',
   borderRadius: '50%',
   justifyContent: 'center',
+
+
   alignItems: 'center',
+  ...(ownerState.active && {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(4, 35, 84) 0%, rgb(4, 35, 84) 50%, rgb(4, 35, 84) 100%)',
+    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+  }),
+  ...(ownerState.completed && {
+    backgroundImage:
+      'linear-gradient( 136deg, rgb(4, 35, 84) 0%, rgb(4, 35, 84) 50%, rgb(4, 35, 84) 100%)',
+  }),
   
-  
-  ...( (ownerState.active )&& {
+
+
+
+  /*
+
+  Este es el que utilicé para que tenga color y utilice el index
+
+  ...( (number==0) && {
     backgroundImage:
     'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
  
@@ -119,32 +136,52 @@ const ColorlibStepIconRoot = styled('div')(({ theme, ownerState, number}) => ({
     backgroundImage:  'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
  
   }),
-  ...((ownerState.active || ownerState.completed ) && number==2&&{
+ ...((number==2  ) &&{
     backgroundImage:
     'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
  
   }),
+
+  //Se supone que esto debería funcionar activandose el color que le corresponde segun su indice
+    y si su etapa ya ha sido completdad o esta activa
   ...((ownerState.active || ownerState.completed ) && number==3 &&{
     backgroundImage:
     'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
   }),
+
+
+  */
 }));
 
 function ColorlibStepIcon(props) {
-  const { active, completed, className, index } = props;
-
+  const { active, completed, className } = props; //Sin el index
+  // Con el index: const { active, completed, className, index } = props;
   const icons = {
+
+    //Aquí poner los íconos que se desean colocar
+
     1: <SendIcon sx={{}}/>,
     2: <AccessTimeIcon />,
     3: <PersonAddAltIcon/>,
-    4: <PersonAddAltIcon/>
+    4: <CheckCircleOutlineIcon/>
   };
 
   return (
+    <ColorlibStepIconRoot ownerState={{ completed, active }}  className={className}>
+      {icons[String(props.icon)]}
+    </ColorlibStepIconRoot>
+  );
+
+    /*
+    Descomentar si se utilizará el index para eleegir color justamente por indice
+      return (
     <ColorlibStepIconRoot ownerState={{ completed, active }} number={index} className={className}>
       {icons[String(props.icon)]}
     </ColorlibStepIconRoot>
   );
+    
+    */
+
 }
 
 ColorlibStepIcon.propTypes = {
@@ -167,7 +204,7 @@ ColorlibStepIcon.propTypes = {
  
 };
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad', 'IDK'];
+const steps = ['Enviado', 'En revisión', 'Delegado', 'Atendido'];
 
 export default function CustomizedSteppers() {
  
@@ -182,7 +219,9 @@ export default function CustomizedSteppers() {
       <Stepper   activeStep={2} connector={ <ColorlibConnector />} orientation="vertical" direction="vertical">
         {steps.map((label, index) => (
           <Step key={label, index}>
-            <StepLabel StepIconComponent={() => <ColorlibStepIcon index={index}/>}>{label}</StepLabel>
+            <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
+            {// Esto utilice para poder cambiar el color: <StepLabel StepIconComponent={() => <ColorlibStepIcon index={index}/>}>{label}</StepLabel>
+            }
           </Step>
         ))}
       </Stepper>
