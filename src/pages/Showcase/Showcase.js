59926f6b2325a8d43b6e9f153c942d0fa36dbecc
mitsuo <rtokumori@pucp.edu.pe>
2021-11-06 23:17:30 -0500

    Add remaining components to Develop

--- src/pages/Showcase/Showcase.js
+++ src/pages/Showcase/Showcase.js
@@ -1,86 +1,84 @@
 /* Author: Mitsuo
  * Date: 2021-11-27
+ * 
+ * Si crean algun componente por favor listarlo aqui para agregarlo cuando
+ * tenga tiempo:
+ * - 
  */
 import React from 'react'
-import ControlForm from './ControlForm'
-import { Paper, Typography, Box, Alert } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
+
 import TypographyPage from "./TypographyPage";
 import ButtonsPage from "./ButtonsPage";
 import TablePage from "./TablePage";
 import UtilPage from "./UtilPage";
 import DTPage from "./DTPage";
 import TestPage from "./TestPage";
-import { useGoogleAuth } from '../Login/googleAuth';
+import TestPage3 from "./TestPage3";
 import TimePickerPage from './TimePickerPage';
+import FormControlsPage from './FormControlsPage';
 
 /* static MUI CSS (export it if it's going to be used a lot) */
 const paperCSS = {
-    m: 5, p: 3
-}
-
-/* extra components to don't evolve return into mess */
-function CenteredTitle(props) {
-    const { text } = props
-    return (
-        /* <Box display="flex" width={1} alignItems="center"> */
-        <Box textAlign="center">
-            <Typography>{text}</Typography>
-        </Box>
-    )
+    m: 5, p: 3,
+    minHeight: "400px"
 }
 
 export default function Employees() {
             
     return (
         <>
-        
-            <Typography variant="h1"  component="div">
+            <Typography variant="h1">
                 Showcasing DreamTeam Components
             </Typography>
 
             <Paper sx={{...paperCSS}} elevation={2}>
-                <CenteredTitle text="Typography"/>
+                <Typography align="center" children="Typography" />
                 <TypographyPage/>
             </Paper>
 
             <Paper sx={{...paperCSS}} elevation={4}>
-                <CenteredTitle text="Form Controls"/>
-                <Alert sx={{ m: 2 }} variant="outlined" severity="info">
-                    Utiliza Hooks,  que se obtienen de {"useForm()"}
-                </Alert>
-                <Alert sx={{m: 2}} variant="outlined" severity="info">
-                    Todo debe estar dentro de {"<Form\>"}
-                </Alert>
-                <ControlForm />
+                <Typography align="center" children="Form Controls" />
+                <FormControlsPage />
             </Paper>
 
             <Paper sx={{...paperCSS}} elevation={6}>
-                <CenteredTitle text="Buttons"/>
+                <Typography align="center" children="Buttons" />
                 <ButtonsPage />
             </Paper>
 
             <Paper sx={{...paperCSS}} elevation={8}>
-                <CenteredTitle text="Table"/>
+                <Typography align="center" children="Table" />
                 <TablePage />
             </Paper>
 
             <Paper sx={{...paperCSS}} elevation={12}>
-                <CenteredTitle text="util"/>
+                <Typography align="center" children="util" />
                 <UtilPage />
             </Paper>
 
             <Paper sx={{...paperCSS}} elevation={14}>
-                <CenteredTitle text="DT (DreamTeam)"/>
+                <Typography align="center" children="DT (DreamTeam)" />
                 <DTPage />
             </Paper>
 
+            {/* TESTS Mitsuo */}
             <Paper sx={{...paperCSS}} elevation={24}>
-                <CenteredTitle text="test"/>
+                <Typography align="center" children="test zone Mitsuo" />
                 <TestPage/>
             </Paper>
+
+            {/* TESTS Gabriela */}
             <Paper sx={{...paperCSS}} elevation={24}>
-                <CenteredTitle text="Picker en un rango determinado"/>
+                <Typography align="center" children="test zone Gabriela" />
+                <Typography children="Picker en un rango determinado"/>
                 <TimePickerPage/>
+            </Paper>
+
+            {/* TESTS Manuel */}
+            <Paper sx={{...paperCSS}} elevation={24}>
+                <Typography align="center" children="test zone Manuel" />
+                <TestPage3/>
             </Paper>
         </>
     )
