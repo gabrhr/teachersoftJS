523027f32bb87ac7285c0dc77063c6c863b9c00e
mitsuo <rtokumori@pucp.edu.pe>
2021-11-18 20:10:05 -0500

    New default for tables is 10

--- src/pages/MesaPartes/RecepcionSolicitud.js
+++ src/pages/MesaPartes/RecepcionSolicitud.js
@@ -1,9 +1,9 @@
 /* Author: Gabriela
  * 
- * Se muestran "Mis Solicitudes".  Desde aqui se puede:
- * - Generar una nueva solicitud.
- * - Ver detalle de una solicitud.
- * "/doc/misSolicitudes"
+ * Se muestran "Solicitudes Secretaria Departamento".  Desde aqui se puede:
+ * - Atender una solicitud directamente desde MP
+ * - Delegar la solicitud a la persona indicada
+ * "/secretaria/mesaPartes/solicitudesGenerales"
  */
 import React, { useState } from 'react'
 import { UserContext } from '../../constants/UserContext'
@@ -11,31 +11,11 @@ import * as MesaPartesService from '../../services/mesaPartesService'
 //Iconos Mesa de Partes
 import DashboardSoli from './DashboardSoli'
 
-/* function createData(id, asunto, descripcion, fecha, autorNombre, estado) {
-    return {
-        id, asunto, descripcion, fecha, autorNombre, estado
-    }
-}
-const usuarios2 = [
-    createData('0', 'Solicitud Descarga 2019', 'Se estima los siguientes docentes ...', '2021-09-30 01:14 pm', 'Caceres', '0'),
-    createData('1', 'Solicitud Descarga 2021', 'Se estima los siguientes docentes ...', '2021-09-30 01:14 pm', 'Caceres', '1'),
-    createData('2', 'Solicitud Descarga 2020', 'Se estima los siguientes docentes ...', '2021-09-30 01:14 pm', 'Caceres', '2'),
-    createData('3', 'Solicitud Descarga 2020', 'Se estima los siguientes docentes ...', '2021-09-30 01:14 pm', 'Caceres', '3'),
-    createData('4', 'Solicitud Descarga 2020', 'Se estima los siguientes docentes ...', '2021-09-30 01:14 pm', 'Caceres', '3'),
-]
- */
-
 function getSolicitudes(setRecords, departamentoID) {
-            //FUNCIONES
-    //MesaPartesService.getSolicitudesByDep(3) 
-    //MesaPartesService.getSolicitudesByIdDel(44) 
-    //MesaPartesService.getSolicitud(33)
-    //MesaPartesService.getSolicitudesByIdSol(user.id)
-    //MesaPartesService.getSolicitudes() 
-    // console.log(user)
     MesaPartesService.getSolicitudesByDep(departamentoID) 
         .then(data => {
             data = data ?? []       // fixes el error raro de mala conexion
+            /* newer first */
             data.sort((x1, x2) => 
                 0 - (new Date(x1.tracking.fecha_enviado) - new Date(x2.tracking.fecha_enviado)))
             setRecords(data)
@@ -47,8 +27,8 @@ export default function MisSolicitudes() {
     const [records, setRecords] = useState([])
     const {user, rol} = React.useContext(UserContext);
     React.useEffect(() => {
-        /* se deberia mandar user.departamento.id */
-        getSolicitudes(setRecords, 3)
+        console.log(user)
+        getSolicitudes(setRecords, user.persona.departamento.id)
     }, [])
 
     return (
