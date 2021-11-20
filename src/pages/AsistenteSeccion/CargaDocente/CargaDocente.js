321093a72be9b884dc53bcbf466df5631dd948d3
Aaron123098 <70769309+Aaron123098@users.noreply.github.com>
2021-11-19 22:10:30 -0500

    preferencia docente sin axios

--- src/pages/AsistenteSeccion/CargaDocente/CargaDocente.js
+++ src/pages/AsistenteSeccion/CargaDocente/CargaDocente.js
@@ -273,12 +273,12 @@ export default function CargaDocente() {
                                   curso: item
                               }
                           }}  style={{ textDecoration: 'none' }}>
-                          <IconButton size="small"
-                            onClick={() => { getRow(item) }}
-                          >
-                            <ArrowForwardIosIcon fontSize="small" />
+                            <IconButton size="small"
+                              onClick={() => { getRow(item) }}
+                            >
+                              <ArrowForwardIosIcon fontSize="small" />
 
-                          </IconButton>
+                            </IconButton>
                           </Link>
                         </StyledTableCell>
 
--- src/pages/AsistenteSeccion/PreferenciaDocente/SolPreferenciaDocentes.js
+++ src/pages/AsistenteSeccion/PreferenciaDocente/SolPreferenciaDocentes.js
@@ -1,7 +1,88 @@
+import { useState } from "react"
+import ContentHeader from "../../../components/AppMain/ContentHeader"
+import AccordionPreferenciaProfesor from './AccordionPreferenciaProfesor'
+import { Controls } from "../../../components/controls/Controls"
+import SearchIcon from '@mui/icons-material/Search';
+import { InputAdornment } from '@mui/material'
+
 export default function SolPreferenciaDocentes(){
+
+    const [profesores, setProfesores] = useState([
+        {
+            foto_URL: '',
+            nombres: 'holo',
+            apellidos: 'k',
+            codigo_pucp: '1',
+            seccion: {
+                nombre: 'Ingeniería Informática'
+            },
+            tipo_docente: 1,
+            preferencia: [
+                {
+                    clave: '22',
+                    nombre_curso: 'MATES'
+                }, 
+                {
+                    clave: '222',
+                    nombre_curso: 'MATES 5'
+                }
+            ]
+        },
+        {
+            foto_URL: '',
+            nombres: 'asdasdas',
+            apellidos: 'kd',
+            codigo_pucp: '13',
+            seccion: {
+                nombre: 'Ingeniería Informática'
+            },
+            tipo_docente: 2,
+            preferencia: [
+                {
+                    clave: '22',
+                    nombre_curso: 'MATES'
+                }, 
+                {
+                    clave: '222',
+                    nombre_curso: 'MATES 5'
+                },
+                {
+                    clave: '22342',
+                    nombre_curso: 'TUTORÍA'
+                }
+            ]
+        }
+    ])
+
+    const [profesoresMostrar, setProfesoresMostrar] = useState(profesores)
+
+
+    const handleSearch = e => {
+        const nuevosProfesores = profesores.filter(x => x.nombres.toLowerCase().includes(e.target.value.toLowerCase()))
+        setProfesoresMostrar(nuevosProfesores)
+    }
+       
+
     return(
         <>
-            
+            <ContentHeader
+            text="Solicitud de preferencias de docentes"
+            cbo={true}
+            />
+            <Controls.Input
+            label="Buscar Docente por Nombre"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            }}
+            sx={{ width: .3, paddingBottom: '1.6%'}}
+            onChange={handleSearch}
+            type="search"
+          />
+            <AccordionPreferenciaProfesor profesores = {profesoresMostrar}/>
         </>
     )
 }
