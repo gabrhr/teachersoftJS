32024e7a2e92a968851fab6ab7348ba016fd3b40
mitsuo <rtokumori@pucp.edu.pe>
2021-11-23 14:48:23 -0500

    Add HeadNotificacion & RegistroForm
    Para el nuevo usuario

--- src/pages/NuevoUsuario/HeadNotificationMisSolicitudes.js
+++ src/pages/NuevoUsuario/HeadNotificationMisSolicitudes.js
@@ -5,11 +5,15 @@
  */
 
 import React from 'react'
+import { Typography, Divider } from '@mui/material'
+import { DT } from '../../components/DreamTeam/DT'
 
 export default function HeadNotificationMisSolicitudes() {
     return (
         <div>
-            
+            <DT.Title size="medium" text="Nuevo Usuario del Sistema"/>
+            <Typography children="Registro de informacion completo, ahora puede enviar su solicitud.  Sin embargo, por motivos de seguridad, solo puede enviar una cantidad limitada de solicitudes." />
+            <Divider sx={{ my: 1 }} />
         </div>
     )
 }
