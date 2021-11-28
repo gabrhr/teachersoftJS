429b3f4f2f84c691cc4a0dd39bf5937a0792a4b4
MBecerra777 <marcelo.becerra@pucp.edu.pe>
2021-11-28 14:09:01 -0500

    Services
    Se agregan los service de procesoDecarga, tramiteDescarga, tramiteSeccionDescarga

---
+++ src/services/tramiteDescargaService.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import url from '../config.js';
+import tokenService from './tokens.js';
+
+export const getTramitesDescarga = async () => {
+    try{
+      //debugger;
+      const request= await axios.get(`${url}/tramitedescarga/`, tokenService.GetTokenPrueba())
+      return request.data;  
+    }catch(exception){
+      console.error(exception);
+    }
+}
+
+export const getTramiteDescarga = async (id) => {
+    try{
+      //debugger;
+      const request= await axios.get(`${url}/tramitedescarga/${id}`,tokenService.GetTokenPrueba(),id);
+      return request.data;  
+    }catch(exception){
+      console.error(exception);
+    }
+}
+
+const registerTramiteDescarga = async newObject => {
+    try{           
+      const request = await axios.post(`${url}/tramitedescarga/`,newObject, tokenService.GetTokenPrueba());
+      console.log(request.data);
+      return request.data; //Es un valor de true o no
+    }catch(exception){
+      console.error(exception);
+    }
+}
+
+const updateTramiteDescarga = async (newObject) => {
+    try{
+      console.log(newObject);
+      const request = await axios.put(`${url}/tramitedescarga/`, newObject, tokenService.GetTokenPrueba());
+      return request.data; //Es un valor de true o no
+    }catch(exception){
+      console.error(exception);
+    }
+}
+
+const deleteTramiteDescarga = async (id) => {
+    try{
+      const request = await axios.delete(`${url}/tramitedescarga/${id}`, tokenService.GetTokenPrueba(),id);
+      return request.data; //Es un valor de true o no
+    }catch(exception){
+      console.error(exception);
+    }
+  }
+
+export default {getTramitesDescarga, getTramiteDescarga, registerTramiteDescarga, updateTramiteDescarga, deleteTramiteDescarga}
