b38f8de6e3c2d0002ddc85ce38ab02a13d72395e
gabrhr <73925454+gabrhr@users.noreply.github.com>
2021-11-29 04:55:45 -0500

    A GestionProcesos
    - ItemActual
    - Conexion axios a mediasxd

--- src/pages/JefeDepartamento/DeudasYDescargas/MantenimientoProcesos/ItemProcesoActualVacio.js
+++ src/pages/JefeDepartamento/DeudasYDescargas/MantenimientoProcesos/ItemProcesoActualVacio.js
@@ -5,17 +5,25 @@
  */
 import React from 'react'
 import { Controls } from '../../../../components/controls/Controls'
-import { Box } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 
 export default function ItemProcesoActualVacio(props) {
     const { setOpenPopup, addOrEdit } = props
     return (
-        <Box border="solid 1px">
-            <Controls.AddButton
-                title="Agregar Nuevo Proceso"
-                variant="iconoTexto"
-                onClick={() => { setOpenPopup(true); }}
-            />
+        <Box border="solid 1px" borderColor="#D4D9EC" borderRadius="15px" 
+            padding={2} mb={2}
+        >
+            <Stack direction="column" align="center" spacing={1}>
+                <Typography display="inline" fontWeight="550"  sx={{color:"primary.light"}}>
+                    No se ha iniciado ningún proceso de descargas
+                </Typography>
+                <Box mb={3}>
+                    <Controls.Button
+                        text="Iniciar Nuevo Proceso"
+                        onClick={() => { setOpenPopup(true); }}
+                    />
+                </Box>
+            </Stack> 
         </Box>
     )
 }
